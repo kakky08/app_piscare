79c1d3fefea16a94a8a8ee698ec56e669d8cec50
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-19 23:32:57 +0900

    プロフィール設定画面のレイアウト変更 #15

---
+++ src/resources/views/mypage/components/sidebar.blade.php
@@ -0,0 +1,27 @@
+<ul class="mypage-sidebar">
+    <li>
+        <a class="nav-link{{--  {{  $page === 'home' ? 'active' : '' }} --}}" aria-current="page" href="{{ route('home.index') }}">
+            <h3 class="ml-2"><i class="fas fa-home mypage-sidebar-icon"></i>Home</h3>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link {{-- {{  $page === 'mypage' ? 'active' : '' }} --}}" aria-current="page" href="{{-- {{ route('profile.show', ['name' => $user->name]) }} --}}">
+            <h3 class="ml-2"><i class="fas fa-user mypage-sidebar-icon"></i>Profile</h3>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link {{-- {{  $page === 'setting' ? 'active' : '' }} --}}" href="{{ route('setting.index') }}">
+            <h3 class="ml-2"><i class="fas fa-cog mypage-sidebar-icon"></i>Setting</h3>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link {{-- {{  $page === 'privacy' ? 'active' : '' }} --}}" href="{{-- {{ route('privacy.index') }} --}}">
+            <h3 class="ml-2"><i class="fas fa-user-shield mypage-sidebar-icon"></i>Privacy</h3>
+        </a>
+    </li>
+    <li>
+        <a class="nav-link {{-- {{  $page === 'data' ? 'active' : '' }} --}}" href="#">
+            <h3 class="ml-2"><i class="fas fa-database mypage-sidebar-icon"></i>Data</h3>
+        </a>
+    </li>
+</ul>
