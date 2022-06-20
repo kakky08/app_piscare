364d68f5662c7f603823a6445b437ed72c858617
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-20 19:56:06 +0900

    投稿レシピ画面のテスト作成 #31

---
+++ src/tests/Feature/PostControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('post.index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('post.index');
+    }
+}
