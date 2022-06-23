de41ee18e463e96e064ac6f7da120a08bd0924d7
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-23 16:26:11 +0900

    楽天レシピのカテゴリ取得用テーブルの作成 #77

---
+++ src/database/migrations/2022_06_23_070227_create_sub_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->BigInteger('id')->unsigned()->primary();
+            $table->BigInteger('categoryId')->unsigned();
+            $table->foreign('categoryId')->references('id')->on('categories')->onDelete('cascade');
+            $table->text('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
