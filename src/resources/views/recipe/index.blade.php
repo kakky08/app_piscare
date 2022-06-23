476a478e3a5b0177b18434eef76809767fd45a7a
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-23 11:52:11 +0900

    楽天レシピから取得したデータを表示 #76

--- src/resources/views/recipe/index.blade.php
+++ src/resources/views/recipe/index.blade.php
@@ -21,33 +21,33 @@
 </ul>
 
 {{-- カード --}}
-{{-- @foreach ($recipes as $key => $recipe)
+@foreach ($recipes as $key => $recipe)
     @if ($key % 4 === 0)
         <div class="row cols-4 spacing-reset card-content">
     @endif
             <div class="card col card-style">
                 <img src={{ $recipe->foodImageUrl }} class="card-img-top card-style-image recipe-card-image" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title card-style-title recipe-card-title">{{ $recipe->title }}</h5>
-                    <p class="card-text card-style-text recipe-card-text">{{ $recipe->description }}</p>
+                    <h5 class="card-title card-style-title recipe-card-title">{{ $recipe->recipeTitle }}</h5>
+                    <p class="card-text card-style-text recipe-card-text">{{ $recipe->recipeDescription }}</p>
                     <div class="recipe-card-like">
-                        <i class="fas fa-heart"></i><p>{{ $recipe->count_likes}}</p>
+                        <i class="fas fa-heart"></i><p>{{-- {{ $recipe->count_likes}} --}}</p>
                     </div>
                     <div class="d-grid">
-                        <a href="{{ route('recipe.show', $recipe->id) }}" class="btn stretched-link card-style-button">詳細</a>
+                        <a href="{{-- {{ route('recipe.show', $recipe->id) }} --}}" class="btn stretched-link card-style-button">詳細</a>
                     </div>
                 </div>
             </div>
     @if (($key + 1) % 4 === 0)
         </div>
     @endif
-@endforeach --}}
+@endforeach
 
 
 
 {{-- ページネーション --}}
-{{-- <nav class="pagination justify-content-center">
+<nav class="pagination justify-content-center">
     {{ $recipes->appends(request()->query())->links() }}
-</nav> --}}
+</nav>
 
 @endsection
