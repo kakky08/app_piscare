748cbcc6409196c3fa86579bf3288d865615544f
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-28 21:59:10 +0900

    記録機能の準備（データベース） #114

---
+++ src/database/migrations/2022_06_28_125219_create_records_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('year_month', 7);
+            $table->string('day', 2);
+            $table->string('is_breakfast')->default(0);
+            $table->string('is_lunch')->default(0);
+            $table->string('is_dinner')->default(0);
+            $table->integer('count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
