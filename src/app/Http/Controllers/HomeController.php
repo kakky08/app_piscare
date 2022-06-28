0d0d896ce3a1dc3782fc85dc6bf7e5cb502e2b39
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-28 21:48:31 +0900

    前の月、翌月に移動できるように機能追加 #113

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -68,4 +68,53 @@ class HomeController extends Controller
 
         return view('mypage.home.index', compact('dates',  'date', 'year_month', 'year', 'month', 'record_year_month', 'record_day'));
     }
+
+    public function moveMonth($move)
+    {
+        // $user = Auth::user();
+
+        $dateStr = sprintf('%s-01', $move);
+
+
+        $date = new Carbon($dateStr);
+        $year_month = substr($dateStr, 0, 7);
+        $year = $date->year;
+        $month = $date->month;
+
+        $record_year_month = substr($dateStr, 0, 7);
+        $today = new Carbon('today');
+        $record_day = $today->day;
+
+        // カレンダーを四角形にするため、前月となる左上の隙間用のデータを入れるためずらす
+        $date->subDay($date->dayOfWeek);
+
+        // 同上。右下の隙間のための計算。
+        $count = 31 + $date->dayOfWeek;
+        $count = ceil($count / 7) * 7;
+        $dates = [];
+
+        for ($i = 0; $i < $count; $i++, $date->addDay()) {
+            // copyしないと全部同じオブジェクトを入れてしまうことになる
+            $dates[] = $date->copy();
+        }
+
+        // 表示されている月の記録を取得
+
+        // $records = Record::where('user_id', Auth::id())->where('year_month', $year_month)->select('day', 'flag_count')->get()->toArray();
+        // $array = array_column($records, 'flag_count', 'day');
+
+        // その日の記録があるかを検索
+        // $today = new Carbon('today');
+        // $day = $today->day;
+
+        // $record = Record::where('user_id', Auth::id())->where('year_month', $record_year_month)->where('day', $record_day)->first();
+
+        /* $action = 'store';
+        if ($record) {
+            $action =  'update';
+        }
+ */
+
+        return view('mypage.home.index', compact('dates',  'date', 'year_month', 'year', 'month',));
+    }
 }
