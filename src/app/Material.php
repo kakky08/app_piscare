eab3b6d6e750599e6fc435f38f1141ad55abeaff
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-06-29 13:41:17 +0900

    投稿レシピの材料の更新(登録)処理の実装の準備 #128

---
+++ src/app/Material.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Material extends Model
+{
+    protected $fillable = [
+        'post_id',
+        'material_name',
+        'quantity',
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo('App\Post');
+    }
+}
---
+++ src/database/migrations/2022_06_29_043922_create_materials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('post_id')->unsigned();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->text('material_name');
+            $table->text('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
