69d7cbda4675a14889e6a3ade0116ed1534b0499
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-07-20 22:17:41 +0900

    mypageのtitleレイアウトの編集 #284

--- src/resources/views/post/components/title.blade.php
+++ src/resources/views/post/components/title.blade.php
@@ -1,4 +1,4 @@
-<div class="row justify-content-between col-lg-12 post-page-title">
+<div class="col-lg-12 post-page-title">
     {{-- タイトル --}}
     <h1 class="col-10 page-title">投稿レシピ</h1>
     <button type="button" class="btn col-2 post-create-button">新しいレシピを投稿する</button>
