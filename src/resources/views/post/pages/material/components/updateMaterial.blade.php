943a20a0a4d61dccc86f13d72fba70473f6b53c0
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-07-20 18:37:40 +0900

    材料更新ページのエラーメッセージとサクセスメッセージの表示 #264

--- src/resources/views/post/pages/material/components/updateMaterial.blade.php
+++ src/resources/views/post/pages/material/components/updateMaterial.blade.php
@@ -4,27 +4,8 @@
     <p class="col">材料の分量</p>
     <div class="col-1"></div>
 </div>
-{{-- @if($errors->has('materials'))
-    <div class="row cols-3 spacing-reset">
-        <div class="col-1"></div>
-        <p class="col alert-message-error">※{{ $errors->first('materials') }}</p>
-        <div class="col-1"></div>
-    </div>
-@endif
-@if($errors->has('materials.*.materialName'))
-    <div class="row cols-3 spacing-reset">
-        <div class="col-1"></div>
-        <p class="col alert-message-error">※{{ $errors->first('materials.*.materialName') }}</p>
-        <div class="col-1"></div>
-    </div>
-@endif
-@if($errors->has('materials.*.quantity'))
-    <div class="row cols-3 spacing-reset">
-        <div class="col-1"></div>
-        <p class="col alert-message-error">※{{ $errors->first('materials.*.quantity') }}</p>
-        <div class="col-1"></div>
-    </div>
-@endif --}}
+@include('post.pages.material.message.updateMaterialError')
+
 <form method="POST" action="{{ route('post.material.update', ['post' => $postId])}}" id="edit-material">
     @method('PUT')
     @csrf
