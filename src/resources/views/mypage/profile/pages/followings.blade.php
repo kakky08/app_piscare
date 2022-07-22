7bb5fddb72f7c0e4ffbe92d4ca352bc6412cda23
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-07-23 00:13:38 +0900

    mypage(プロフィール)のフォロー・フォロワーレイアウトの変更 #287

--- src/resources/views/mypage/profile/pages/followings.blade.php
+++ src/resources/views/mypage/profile/pages/followings.blade.php
@@ -8,7 +8,32 @@
 @section('main')
     @include('mypage.profile.components.profile')
     @include('mypage.profile.components.tabs', ['isPosts' => false, 'isLikes' => false, 'isFollowings' => true, 'isFollowers' => false])
-    @foreach($followings as $following)
-        {{ $following->id }}
-    @endforeach
+    <ul class="follow-list">
+        @foreach($followings as $following)
+            <li class="follow-item">
+                <a href="{{route('information.show', ['name' => $following->name ])}}" class="follow-item-link">
+                    @if (empty($following->icon))
+                        <img src="{{ asset('images/yellowtail.png') }}" class="follow-item-image" alt="{{$user->name}}の初期アイコン">
+                    @else
+                        <i class="fas fa-user-circle fa-10x"></i>
+                    @endif
+                    <p class="follow-item-name">{{ $following->name }}</p>
+                    <follow-button
+                    class="ml-auto"
+                    :initial-is-followed-by='@json($following->isFollowedBy(Auth::user()))'
+                    :authorized='@json(Auth::check())'
+                    endpoint='{{ route('user.follow', ['id' => $user->id]) }}'
+                    >
+                    </follow-button>
+                </a>
+            </li>
+        @endforeach
+         <li class="follow-item">
+                <a href="" class="follow-item-link">
+                    <img src="" class="follow-item-image" alt="初期アイコン">
+                    <p class="follow-item-name">あいうえおあいうえお</p>
+                    <button>test</button>
+                </a>
+            </li>
+    </ul>
 @endsection
