137297f056296fab2101606510489015fb945352
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-07-23 21:14:12 +0900

    インフォメーションのフォロー・フォロワーレイアウトの変更 #288

--- src/resources/views/information/pages/followings.blade.php
+++ src/resources/views/information/pages/followings.blade.php
@@ -6,6 +6,22 @@
     @include('information.components.profile')
     @include('information.components.tabs', ['isPosts' => false, 'isLikes' => false, 'isFollowings' => true, 'isFollowers' => false])
     @foreach($followings as $following)
-        {{ $following->id }}
+        <li class="follow-item">
+            <a href="{{route('information.show', ['name' => $following->name ])}}" class="follow-item-link">
+                @if (empty($following->icon))
+                    <img src="{{ asset('images/yellowtail.png') }}" class="follow-item-image" alt="{{$user->name}}の初期アイコン">
+                @else
+                    <i class="fas fa-user-circle fa-10x"></i>
+                @endif
+                <p class="follow-item-name">{{ $following->name }}</p>
+                <follow-button
+                class="ml-auto"
+                :initial-is-followed-by='@json($following->isFollowedBy(Auth::user()))'
+                :authorized='@json(Auth::check())'
+                endpoint='{{ route('user.follow', ['id' => $user->id]) }}'
+                >
+                </follow-button>
+            </a>
+        </li>
     @endforeach
 @endsection
