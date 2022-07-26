ade34ec77d7a38af9853280bd6e153bce5e56597
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-07-26 23:26:05 +0900

    ソーシャルログイン（twitter）の追加 #322

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -88,7 +88,7 @@
                         </a>
                     </div>
                     <div class="d-grid gap-2 col-8 mx-auto auth-button-social">
-                        <a href="{{-- {{ route('login.{provider}', ['provider' => 'twitter']) }} --}}" class="btn btn-block auth-button-twitter auth-button-social-link">
+                        <a href="{{ route('login.{provider}', ['provider' => 'twitter']) }}" class="btn btn-block auth-button-twitter auth-button-social-link">
                             <i class="fab fa-twitter auth-button-social-icon"></i>Twitterでログイン
                         </a>
                     </div>
