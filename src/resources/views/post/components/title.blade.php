846eac06d75ea541afa1331db5d75ced4b2f595d
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-07-26 23:13:47 +0900

    投稿レシピページの投稿作成ボタンのrootを設定 #320

--- src/resources/views/post/components/title.blade.php
+++ src/resources/views/post/components/title.blade.php
@@ -1,5 +1,5 @@
 <div class="col-lg-12 post-page-title">
     {{-- タイトル --}}
     <h1 class="col-10 page-title">投稿レシピ</h1>
-    <button type="button" class="btn col-2 post-create-button">新しいレシピを投稿する</button>
+    <a href="{{ route('post.create') }}" class="btn post-create-button">新しいレシピを投稿する</a>
 </div>
