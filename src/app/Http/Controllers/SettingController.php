44a9b87ee4a2f3f5556c03f0c16c26621acb6642
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-08-03 00:06:00 +0900

    アイコン変更処理の実装 #373

--- src/app/Http/Controllers/SettingController.php
+++ src/app/Http/Controllers/SettingController.php
@@ -7,9 +7,11 @@ use App\Http\Requests\UpdatePasswordRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Storage;
 
 class SettingController extends Controller
 {
+
     public function index()
     {
         return view('mypage.setting.index');
@@ -17,13 +19,11 @@ class SettingController extends Controller
 
     public function updateEmail(UpdateEmailRequest $request, User $user)
     {
-        dd('test');
-
         $user->fill($request->all())->save();
         return redirect()->route('setting.index')->with('completion-of-update-email', 'メールアドレスの更新が完了しました。');
     }
 
-    public function updatePassword(UpdatePasswordRequest $request)
+    /* public function updatePassword(UpdatePasswordRequest $request)
     {
 dd('test');
 $usre = Auth::user();
@@ -36,5 +36,19 @@ $usre = Auth::user();
         $user->save();
 
         return redirect()->route('setting.index')->with('conpletion-of-update-password', 'パスワードの更新が完了しました。');
+    } */
+
+    public function updateIcon(Request $request, User $user)
+    {
+
+        if(isset($user->icon))
+        {
+            Storage::disk('s3')->delete($user->icon);
+        }
+        $image = $request->file('file');
+        $path = Storage::disk('s3')->putFile('/', $image, 'public');
+        $user->icon = $path;
+        $user->save();
+        return redirect()->route('setting.index');
     }
 }
