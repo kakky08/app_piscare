cdee31dfc2edadc72cdd90623b68f96c3c886516
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-08-04 11:36:37 +0900

    AWS SESを使ったメール送信機能の実装 #374

--- src/config/mail.php
+++ src/config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'ses'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', null),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'piscare@piscare.net'),
+        'name' => env('MAIL_FROM_NAME', 'Piscare'),
     ],
 
     /*
