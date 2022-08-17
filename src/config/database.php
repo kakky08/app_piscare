1889c5e254d1af210059c950d60f4581f74fb1e3
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2022-08-17 22:52:54 +0900

    キャッシュを利用する(npm) #418

--- src/config/database.php
+++ src/config/database.php
@@ -95,7 +95,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'db_name',
+            'database' => 'laravel-test',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
